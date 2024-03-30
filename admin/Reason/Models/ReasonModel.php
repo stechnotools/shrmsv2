84f020a379d24e0a959d960bb05a0343c5043569
niranjan-wassan <niranjan@wassan.org>
2024-03-30 17:22:30 +0530

    add reason

---
+++ admin/Reason/Models/ReasonModel.php
@@ -0,0 +1,110 @@
+<?php
+namespace Admin\Reason\Models;
+use CodeIgniter\Model;
+
+class ReasonModel extends Model
+{
+
+    protected $DBGroup              = 'default';
+    protected $table                = 'reason';
+    protected $primaryKey           = 'id';
+    protected $useAutoIncrement     = true;
+    protected $insertID             = 0;
+    protected $returnType           = 'object';
+    protected $useSoftDelete        = true;
+    protected $protectFields        = false;
+    protected $allowedFields        = [];
+
+    // Dates
+    protected $useTimestamps        = true;
+    protected $dateFormat           = 'datetime';
+    protected $createdField         = 'created_at';
+    protected $updatedField         = 'updated_at';
+    protected $deletedField         = 'deleted_at';
+
+
+    // Validation
+    protected $validationRules      = [
+        'name' => array(
+            'label' => 'Name',
+            'rules' => 'trim|required|max_length[100]'
+        ),
+
+
+    ];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks       = true;
+    protected $beforeInsert         = [];
+    protected $afterInsert          = [];
+    protected $beforeUpdate         = [];
+    protected $afterUpdate          = [];
+    protected $beforeFind           = [];
+    protected $afterFind            = [];
+    protected $beforeDelete         = [];
+    protected $afterDelete          = [];
+
+
+    public function __construct(){
+		parent::__construct();
+	}
+
+    public function getAll($data = array()){
+        $builder=$this->db->table("{$this->table} r");
+        $this->filter($builder,$data);
+
+        $builder->select("r.*");
+
+        if (isset($data['sort']) && $data['sort']) {
+            $sort = $data['sort'];
+        } else {
+            $sort = "r.name";
+        }
+
+        if (isset($data['order']) && ($data['order'] == 'desc')) {
+            $order = "desc";
+        } else {
+            $order = "asc";
+        }
+        $builder->orderBy($sort, $order);
+
+        if (isset($data['start']) || isset($data['limit'])) {
+            if ($data['start'] < 0) {
+                $data['start'] = 0;
+            }
+
+            if ($data['limit'] < 1) {
+                $data['limit'] = 10;
+            }
+            $builder->limit((int)$data['limit'],(int)$data['start']);
+        }
+
+        $res = $builder->get()->getResult();
+
+        return $res;
+    }
+
+    public function getTotal($data = array()) {
+        $builder=$this->db->table("{$this->table} r");
+        $this->filter($builder,$data);
+        $count = $builder->countAllResults();
+        return $count;
+    }
+
+    private function filter($builder,$data){
+        $builder->where("r.$this->deletedField", null);
+        if (!empty($data['filter_search'])) {
+            $builder->where("
+				name LIKE '%{$data['filter_search']}%'
+				OR code LIKE '%{$data['filter_search']}%')"
+            );
+        }
+    }
+
+
+
+
+}
